4d57d066376ae30779f02a477145b91388e9c32c
mrhorst <mhorst31@gmail.com>
2025-04-23 21:19:48 -0400

    create Project class

---
+++ Project.js
@@ -0,0 +1,22 @@
+import Todo from './Todo.js'
+
+class Project {
+  constructor(title = '', description = '', dueDate = '') {
+    this.title = title
+    this.description = description
+    this.dueDate = dueDate
+    this.todos = []
+  }
+
+  changeTitle() {}
+
+  changeDescription() {}
+
+  changeDueDate() {}
+
+  addTodo(todo = new Todo()) {
+    this.todos.push(todo)
+  }
+}
+
+export default Project
