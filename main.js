d484bb5feae0aa828bdccf9de62956005e1f745f
mrhorst <mhorst31@gmail.com>
2025-04-25 12:55:50 -0400

    add projects to localstorage in a very dummy way. will refactor later

--- main.js
+++ main.js
@@ -4,15 +4,6 @@ import Project from './Project.js'
 const addProjectBtn = document.querySelector('#proj-btn')
 const addTodoBtn = document.querySelector('#todo-btn')
 
-addProjectBtn.addEventListener('click', (e) => {
-  e.preventDefault()
-  const { projTitle, projDesc, projDueDate } = getProjectData()
-  const project = new Project(projTitle, projDesc, projDueDate)
-
-  const ul = document.querySelector('#projUl')
-  ul.appendChild(addList(project))
-})
-
 const getProjectData = () => {
   const projTitle = document.querySelector('#projTitle').value
   const projDesc = document.querySelector('#projDesc').value
@@ -26,5 +17,44 @@ const addList = ({ id, title, description, dueDate }) => {
   li.textContent = `${title}: ${description}. Due date: ${dueDate}`
   return li
 }
+
+const loadProjects = (projects) => {
+  const ul = document.querySelector('#projUl')
+  projects.forEach((project) => {
+    ul.appendChild(addList(project))
+  })
+}
+
+const addProjToLocalstorage = (project) => {
+  const stored = window.localStorage.getItem('Projects')
+  const listOfProjects = stored ? JSON.parse(stored) : []
+
+  listOfProjects.push(project)
+
+  window.localStorage.setItem('Projects', JSON.stringify(listOfProjects))
+}
+
+const clearLi = () => {
+  const allLi = document.querySelectorAll('#projUl li')
+  allLi.forEach((li) => {
+    li.remove()
+  })
+}
+
+addProjectBtn.addEventListener('click', (e) => {
+  e.preventDefault()
+  const { projTitle, projDesc, projDueDate } = getProjectData()
+  const project = new Project(projTitle, projDesc, projDueDate)
+
+  addProjToLocalstorage(project)
+  clearLi()
+  loadProjects(getStoredProjectsArray())
+})
+
+const getStoredProjectsArray = () => {
+  return JSON.parse(window.localStorage.getItem('Projects'))
+}
+
+loadProjects(getStoredProjectsArray())
 
 export { Todo, Project }
